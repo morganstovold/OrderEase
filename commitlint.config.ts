91bb10f087cafb6137b9af8f6ab2f2ed7c247883
Morgan Stovold <morganstovold1@icloud.com>
2024-04-29 23:01:28 -0700

    fix(commitlint): increased body-max-line-length -> 200

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,10 @@
 import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [2, 'always', 200]
+  }
 };
 
 export default Configuration;
